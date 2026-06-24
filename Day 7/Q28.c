cb76798f427789cae83e6f46e076ccf85772d2c4
Khushi200829 <25110CN531@glbitm.ac.in>
2026-06-24 09:42:07 +0530

    Added Day 7

---
+++ Day 7/Q28.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int rev = 0;
+
+int reverse(int n)
+{
+    if(n == 0)
+        return rev;
+
+    rev = rev * 10 + n % 10;
+
+    reverse(n / 10);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    reverse(n);
+
+    printf("Reverse = %d", rev);
+
+    return 0;
+}
