ac98d096efbd9bfaab67b9131f9bf39fb18d84e9
Khushi200829 <25110CN531@glbitm.ac.in>
2026-07-06 12:37:43 +0530

    Added Day 18

---
+++ Day 18/Q71.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50, 60, 70};
+    int n = 7;
+    int key = 40;
+    int low = 0, high = n - 1, mid;
+    int found = 0;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            printf("Element found at position %d", mid + 1);
+            found = 1;
+            break;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (found == 0) {
+        printf("Element not found");
+    }
+
+    return 0;
+}
